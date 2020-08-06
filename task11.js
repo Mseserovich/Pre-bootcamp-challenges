13b759d29eaccba6f38310a6c865e7c8dca9d4cd
Mseserovich <tshepo.tshabalala@moov.life>
2020-08-06 21:41:59 +0200

    task11 fix

--- task11.js
+++ task11.js
@@ -3,8 +3,9 @@ function twoStrings(str1, str2){
     str1 = [...str1];
     //str22 = [...str2];
     str1.forEach(element => {
-        let check = str2.indexOf(element)
-        if (check >= 0){
+        let check = str2.indexOf(element);
+        let check2 = val.indexOf(element);
+        if (check >= 0 && !(check2 >= 0)){
             //console.log(str2.indexOf(element));
             val.push(element);
             }
@@ -12,4 +13,4 @@ function twoStrings(str1, str2){
     console.log(`Common letters: ${val.join('')}`);
 }
 
-twoStrings('house', 'computers');
+twoStrings('houusee', 'compuuters');
