7e117751af1f9ce5115e070e18791dcede3f5c62
Mseserovich <tshepo.tshabalala@moov.life>
2020-08-20 09:21:50 +0200

    fixed task4 bug

--- task4.js
+++ task4.js
@@ -1,10 +1,15 @@
 function checkNum(num1, num2){
     let sum = [num1 + num2].join('');
+    num1 = [num1].join('')
+    num2 = [num2].join('')
     
     for (let i = 0; i < sum.length; i++){
-        console.log(sum[i]);
-        if ((num2 === 3 || num1 === 3) && sum[i] === '3'){
-            return true;
+        if (num2[i] == '3' || num1[i] == '3'){
+            for (let j = 0; j < sum.length; j++){
+                if (sum[j] == '3'){
+                    return true;
+                }
+            }
         }
     }
     return false;
